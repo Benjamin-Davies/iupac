e1af75fe15a559b3e3aea10a41177232c425cc3a
Benjamin Davies <bentendavies@gmail.com>
2024-07-01 23:21:53 +1200

    Speed up simulation

--- src/bin/structural_formula.rs
+++ src/bin/structural_formula.rs
@@ -10,13 +10,15 @@ use iupac::{graph::Graph, parser::parse, Element};
 
 const FONT_SIZE: f32 = 32.0;
 const BOND_DRAWING_EXCLUSION_RADIUS: f32 = 0.5 * FONT_SIZE;
+const ATOM_ADDITION_RATE: f32 = 10.0;
 const ITER_PER_FIXED_UPDATE: usize = 100;
+const HIDE_HYDROGENS: bool = true;
 
 const STEP_SIZE: f32 = 0.5;
 const BOND_STIFFNESS: f32 = 1.0;
 const HYDROGEN_BOND_TARGET_LENGTH: f32 = 1.5 * FONT_SIZE;
 const BOND_TARGET_LENGTH: f32 = 2.0 * FONT_SIZE;
-const ATOM_REPULSION: f32 = 20.0;
+const ATOM_REPULSION: f32 = 10.0;
 const BOND_REPULSION: f32 = 2000.0;
 const CENTER_PULL: f32 = 0.2;
 
@@ -148,11 +150,11 @@ fn draw(
     }
 
     let molecule = molecule.single();
-    let max_i = LAST_SHOWN_ATOM.load(Ordering::SeqCst);
+    let g = &molecule.graph;
 
     for bond in &molecule.graph.bonds {
         let &(a, b) = bond;
-        if a > max_i || b > max_i {
+        if skip(g, a) || skip(g, b) {
             continue;
         }
         let a = molecule.atoms[a];
@@ -168,10 +170,10 @@ fn draw(
 
     for (i, &atom) in molecule.atoms.iter().enumerate() {
         let mut visibility = visibilities.get_mut(atom).unwrap();
-        if i <= max_i {
-            *visibility = Visibility::Inherited;
-        } else {
+        if skip(g, i) {
             *visibility = Visibility::Hidden;
+        } else {
+            *visibility = Visibility::Inherited;
         }
     }
 }
@@ -185,16 +187,17 @@ fn gradient_descent(
     time: Res<Time>,
 ) {
     let molecule = molecule.single();
+    let g = &molecule.graph;
     let mut atom_positions = molecule
         .atoms
         .iter()
         .map(|&e| atoms.get(e).unwrap().1.translation.xy())
         .collect::<Vec<_>>();
 
-    let max_index = time.elapsed_seconds() as usize;
+    let max_index = (time.elapsed_seconds() * ATOM_ADDITION_RATE) as usize;
     LAST_SHOWN_ATOM.store(max_index, Ordering::SeqCst);
 
-    let start_of_index = max_index as f32;
+    let start_of_index = max_index as f32 / ATOM_ADDITION_RATE;
     let first_step_for_index = (time.elapsed_seconds() - start_of_index) < time.delta_seconds();
     if first_step_for_index {
         let new_i = max_index;
@@ -205,15 +208,21 @@ fn gradient_descent(
     }
 
     for _ in 0..ITER_PER_FIXED_UPDATE {
-        let cost_gradient = cost_gradient(&molecule.graph, &atom_positions, max_index);
+        let cost_gradient = cost_gradient(&molecule.graph, &atom_positions);
         for i in 0..atom_positions.len() {
             atom_positions[i] -= STEP_SIZE * cost_gradient[i];
         }
     }
-    let cost = cost(&molecule.graph, &atom_positions, max_index);
+    let cost = cost(&molecule.graph, &atom_positions);
 
-    let center = atom_positions.iter().take(max_index + 1).sum::<Vec2>()
-        / atom_positions.len().min(max_index + 1) as f32;
+    let (sum, count) = atom_positions
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| !skip(g, i))
+        .fold((Vec2::ZERO, 0), |(sum, count), (_, &position)| {
+            (sum + position, count + 1)
+        });
+    let center = sum / count as f32;
     for position in atom_positions.iter_mut().take(max_index + 1) {
         *position -= CENTER_PULL * center;
     }
@@ -260,33 +269,32 @@ fn place_new_atom(graph: &Graph, atom_positions: &[Vec2], new_i: usize, max_inde
     position + predictable_noise
 }
 
-fn cost(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> f32 {
+fn cost(g: &Graph, atom_positions: &[Vec2]) -> f32 {
     // Model the cost as the potential energy of a mechanical system
     let mut energy = 0.0;
 
     // Model bonds as springs
-    for bond in &graph.bonds {
+    for bond in &g.bonds {
         let &(i, j) = bond;
-        if i > max_index || j > max_index {
+        if skip(g, i) || skip(g, j) {
             continue;
         }
 
         let u_vec = atom_positions[j] - atom_positions[i];
         let u = u_vec.length();
-        let target_length =
-            if graph.atoms[i] == Element::Hydrogen || graph.atoms[j] == Element::Hydrogen {
-                HYDROGEN_BOND_TARGET_LENGTH
-            } else {
-                BOND_TARGET_LENGTH
-            };
+        let target_length = if g.atoms[i] == Element::Hydrogen || g.atoms[j] == Element::Hydrogen {
+            HYDROGEN_BOND_TARGET_LENGTH
+        } else {
+            BOND_TARGET_LENGTH
+        };
         let x = u - target_length;
         energy += 0.5 * BOND_STIFFNESS * x.powi(2);
     }
 
     // Model atoms as repelling charges
-    for i in 0..graph.atoms.len() {
-        for j in i + 1..graph.atoms.len() {
-            if i > max_index || j > max_index {
+    for i in 0..g.atoms.len() {
+        for j in i + 1..g.atoms.len() {
+            if skip(g, i) || skip(g, j) {
                 continue;
             }
 
@@ -297,8 +305,8 @@ fn cost(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> f32 {
     }
 
     // Model bonds as repelling charges
-    foreach_adjacent_bond_pair(graph, |(a, b), (c, d)| {
-        if a > max_index || b > max_index || c > max_index || d > max_index {
+    foreach_adjacent_bond_pair(g, |(a, b), (c, d)| {
+        if skip(g, a) || skip(g, b) || skip(g, c) || skip(g, d) {
             return;
         }
 
@@ -316,23 +324,22 @@ fn cost(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> f32 {
     energy
 }
 
-fn cost_gradient(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> Vec<Vec2> {
+fn cost_gradient(g: &Graph, atom_positions: &[Vec2]) -> Vec<Vec2> {
     let mut energy_gradient = vec![Vec2::ZERO; atom_positions.len()];
 
-    for bond in &graph.bonds {
+    for bond in &g.bonds {
         let &(i, j) = bond;
-        if i > max_index || j > max_index {
+        if skip(g, i) || skip(g, j) {
             continue;
         }
 
         let u_vec = atom_positions[j] - atom_positions[i];
         let u = u_vec.length();
-        let target_length =
-            if graph.atoms[i] == Element::Hydrogen || graph.atoms[j] == Element::Hydrogen {
-                HYDROGEN_BOND_TARGET_LENGTH
-            } else {
-                BOND_TARGET_LENGTH
-            };
+        let target_length = if g.atoms[i] == Element::Hydrogen || g.atoms[j] == Element::Hydrogen {
+            HYDROGEN_BOND_TARGET_LENGTH
+        } else {
+            BOND_TARGET_LENGTH
+        };
         let x = u - target_length;
         let dx_by_du_vec = u_vec / u;
         let denergy_by_du_vec = BOND_STIFFNESS * x * dx_by_du_vec;
@@ -340,9 +347,9 @@ fn cost_gradient(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> Ve
         energy_gradient[i] -= denergy_by_du_vec;
     }
 
-    for i in 0..graph.atoms.len() {
-        for j in i + 1..graph.atoms.len() {
-            if i > max_index || j > max_index {
+    for i in 0..g.atoms.len() {
+        for j in i + 1..g.atoms.len() {
+            if skip(g, i) || skip(g, j) {
                 continue;
             }
 
@@ -355,8 +362,8 @@ fn cost_gradient(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> Ve
         }
     }
 
-    foreach_adjacent_bond_pair(graph, |(a, b), (c, d)| {
-        if a > max_index || b > max_index || c > max_index || d > max_index {
+    foreach_adjacent_bond_pair(g, |(a, b), (c, d)| {
+        if skip(g, a) || skip(g, b) || skip(g, c) || skip(g, d) {
             return;
         }
 
@@ -380,17 +387,28 @@ fn cost_gradient(graph: &Graph, atom_positions: &[Vec2], max_index: usize) -> Ve
     energy_gradient
 }
 
-fn foreach_adjacent_bond_pair(
-    graph: &Graph,
-    mut callback: impl FnMut((usize, usize), (usize, usize)),
-) {
-    for i in 0..graph.bonds.len() {
-        for j in i + 1..graph.bonds.len() {
-            let (a, b) = graph.bonds[i];
-            let (c, d) = graph.bonds[j];
+fn foreach_adjacent_bond_pair(g: &Graph, mut callback: impl FnMut((usize, usize), (usize, usize))) {
+    for i in 0..g.bonds.len() {
+        for j in i + 1..g.bonds.len() {
+            let (a, b) = g.bonds[i];
+            let (c, d) = g.bonds[j];
             if a == c || a == d || b == c || b == d {
-                callback(graph.bonds[i], graph.bonds[j]);
+                callback(g.bonds[i], g.bonds[j]);
             }
         }
     }
+}
+
+fn skip(g: &Graph, index: usize) -> bool {
+    let max_index = LAST_SHOWN_ATOM.load(Ordering::SeqCst);
+    if index > max_index {
+        return true;
+    }
+
+    let element = g.atoms[index];
+    if HIDE_HYDROGENS && element == Element::Hydrogen {
+        return true;
+    }
+
+    false
 }
